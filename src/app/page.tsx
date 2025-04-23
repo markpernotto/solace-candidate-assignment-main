1205212bb4d3734bee51a59b12d6dd7ea6de482e
Mark Pernotto <mark@pernotto.com>
2025-04-22 20:41:33 -0700

    fix: hydration issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,10 @@ import { useEffect, useState } from "react";
 
 export default function Home() {
   const [advocates, setAdvocates] = useState([]);
-  const [filteredAdvocates, setFilteredAdvocates] = useState([]);
+  const [
+    filteredAdvocates,
+    setFilteredAdvocates,
+  ] = useState([]);
 
   useEffect(() => {
     console.log("fetching advocates...");
@@ -19,19 +22,31 @@ export default function Home() {
   const onChange = (e) => {
     const searchTerm = e.target.value;
 
-    document.getElementById("search-term").innerHTML = searchTerm;
+    document.getElementById(
+      "search-term",
+    ).innerHTML = searchTerm;
 
     console.log("filtering advocates...");
-    const filteredAdvocates = advocates.filter((advocate) => {
-      return (
-        advocate.firstName.includes(searchTerm) ||
-        advocate.lastName.includes(searchTerm) ||
-        advocate.city.includes(searchTerm) ||
-        advocate.degree.includes(searchTerm) ||
-        advocate.specialties.includes(searchTerm) ||
-        advocate.yearsOfExperience.includes(searchTerm)
-      );
-    });
+    const filteredAdvocates = advocates.filter(
+      (advocate) => {
+        return (
+          advocate.firstName.includes(
+            searchTerm,
+          ) ||
+          advocate.lastName.includes(
+            searchTerm,
+          ) ||
+          advocate.city.includes(searchTerm) ||
+          advocate.degree.includes(searchTerm) ||
+          advocate.specialties.includes(
+            searchTerm,
+          ) ||
+          advocate.yearsOfExperience.includes(
+            searchTerm,
+          )
+        );
+      },
+    );
 
     setFilteredAdvocates(filteredAdvocates);
   };
@@ -49,22 +64,30 @@ export default function Home() {
       <div>
         <p>Search</p>
         <p>
-          Searching for: <span id="search-term"></span>
+          Searching for:{" "}
+          <span id="search-term"></span>
         </p>
-        <input style={{ border: "1px solid black" }} onChange={onChange} />
-        <button onClick={onClick}>Reset Search</button>
+        <input
+          style={{ border: "1px solid black" }}
+          onChange={onChange}
+        />
+        <button onClick={onClick}>
+          Reset Search
+        </button>
       </div>
       <br />
       <br />
       <table>
         <thead>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>City</th>
-          <th>Degree</th>
-          <th>Specialties</th>
-          <th>Years of Experience</th>
-          <th>Phone Number</th>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>City</th>
+            <th>Degree</th>
+            <th>Specialties</th>
+            <th>Years of Experience</th>
+            <th>Phone Number</th>
+          </tr>
         </thead>
         <tbody>
           {filteredAdvocates.map((advocate) => {
@@ -75,11 +98,15 @@ export default function Home() {
                 <td>{advocate.city}</td>
                 <td>{advocate.degree}</td>
                 <td>
-                  {advocate.specialties.map((s) => (
-                    <div>{s}</div>
-                  ))}
+                  {advocate.specialties.map(
+                    (s) => (
+                      <div>{s}</div>
+                    ),
+                  )}
                 </td>
-                <td>{advocate.yearsOfExperience}</td>
+                <td>
+                  {advocate.yearsOfExperience}
+                </td>
                 <td>{advocate.phoneNumber}</td>
               </tr>
             );
