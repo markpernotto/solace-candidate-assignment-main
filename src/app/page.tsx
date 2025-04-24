a43522c9f48c02b157f530a82dc9eef744afbc9c
Mark Pernotto <mark@pernotto.com>
2025-04-23 22:54:45 -0700

    feat: implement useSWR

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import {
   useState,
   useRef,
 } from "react";
+import useSWR from "swr";
 
 export interface Advocate {
   id: number;
@@ -18,26 +19,36 @@ export interface Advocate {
 }
 
 export default function Home() {
-  const [advocates, setAdvocates] = useState([]);
+  const [advocates, setAdvocates] = useState<
+    Advocate[]
+  >([]);
   const [searchTerm, setSearchTerm] =
     useState("");
   const searchInputRef =
     useRef<HTMLInputElement>(null);
 
-  useEffect(() => {
-    console.log("fetching advocates...");
-    const searchQueryParameter =
-      searchTerm.length > 0
-        ? `?search=${searchTerm}`
-        : "";
-    fetch(
-      `http://localhost:3000/api/advocates${searchQueryParameter}`,
-    ).then((response) => {
-      response.json().then((jsonResponse) => {
-        setAdvocates(jsonResponse.results ?? []);
-      });
-    });
-  }, [searchTerm]);
+  interface AdvocatesApiResponse {
+    results: Advocate[];
+  }
+
+  const { data, error, isLoading } = useSWR<
+    Advocate[]
+  >(
+    `http://localhost:3000/api/advocates${
+      searchTerm ? `?search=${searchTerm}` : ""
+    }`,
+    async (url: string): Promise<Advocate[]> =>
+      fetch(url)
+        .then(
+          (response: Response) =>
+            response.json() as Promise<AdvocatesApiResponse>,
+        )
+        .then((data: AdvocatesApiResponse) => {
+          console.log("data", data);
+          setAdvocates(data.results);
+          return data.results;
+        }),
+  );
 
   return (
     <main style={{ margin: "24px" }}>
@@ -74,42 +85,49 @@ export default function Home() {
       </div>
       <br />
       <br />
-      <table>
-        <thead>
-          <tr>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>City</th>
-            <th>Degree</th>
-            <th>Specialties</th>
-            <th>Years of Experience</th>
-            <th>Phone Number</th>
-          </tr>
-        </thead>
-        <tbody>
-          {advocates.map((advocate: Advocate) => {
-            return (
-              <tr key={advocate.id}>
-                <td>{advocate.firstName}</td>
-                <td>{advocate.lastName}</td>
-                <td>{advocate.city}</td>
-                <td>{advocate.degree}</td>
-                <td>
-                  {advocate.specialties.map(
-                    (s, i) => (
-                      <div key={i}>{s}</div>
-                    ),
-                  )}
-                </td>
-                <td>
-                  {advocate.yearsOfExperience}
-                </td>
-                <td>{advocate.phoneNumber}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      {error && <div>There is an error</div>}
+      {data && data.length > 0 ? (
+        <table>
+          <thead>
+            <tr>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>City</th>
+              <th>Degree</th>
+              <th>Specialties</th>
+              <th>Years of Experience</th>
+              <th>Phone Number</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data?.map((advocate: Advocate) => {
+              return (
+                <tr key={advocate.id}>
+                  <td>{advocate.firstName}</td>
+                  <td>{advocate.lastName}</td>
+                  <td>{advocate.city}</td>
+                  <td>{advocate.degree}</td>
+                  <td>
+                    {advocate.specialties.map(
+                      (s, i) => (
+                        <div key={i}>{s}</div>
+                      ),
+                    )}
+                  </td>
+                  <td>
+                    {advocate.yearsOfExperience}
+                  </td>
+                  <td>{advocate.phoneNumber}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      ) : isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        !error && <div>No Results Found</div>
+      )}
     </main>
   );
 }
